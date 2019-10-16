e8c72592b39f2eb411e52baaf7d20a8d4672e5ee
rendi <r3ndi88@yahoo.com>
2019-10-16 15:56:01 +0700

    change project dir di katalon-execute

--- katalon/src/scripts/katalon-execute.sh
+++ katalon/src/scripts/katalon-execute.sh
@@ -21,7 +21,8 @@ if ! [ -d "$source_dir" ]; then
 fi
 
 # project source code
-project_dir=/tmp/katalon_execute/project
+#project_dir=/tmp/katalon_execute/project
+project_dir=../../../test/project
 mkdir -p $project_dir
 
 cp -r $source_dir/* $project_dir
